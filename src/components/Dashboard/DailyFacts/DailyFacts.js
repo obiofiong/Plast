2afbfb1f97a46506f1426cdcb523668359c895e1
John <johnobiofiong1@gmail.com>
2021-07-16 18:56:18 +0100

    new features

--- src/components/Dashboard/DailyFacts/DailyFacts.js
+++ src/components/Dashboard/DailyFacts/DailyFacts.js
@@ -7,7 +7,7 @@ function DailyFacts() {
 			className="bg-gray-100 p-1 text-start 
             ">
 			<p className="font-bold text-gray-600 ml-10">Facts of the Day</p>
-			<div className="my-3 px-3 flex items-center w-full overflow-x-scroll scrollbar-hide">
+			<div className="my-3 px-3 flex items-center justify-start w-full overflow-x-scroll scrollbar-hide">
 				<DailyFact />
 				{/* <DailyFact /> */}
 				{/* <DailyFact /> */}
